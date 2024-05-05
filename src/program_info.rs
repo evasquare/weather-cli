a28cdbb16f95c2dd193f4c495e3b744ebba87a5d
cosmostellar <160427240+cosmostellar@users.noreply.github.com>
2024-05-05 22:51:04 +0900

    Update program description

--- src/program_info.rs
+++ src/program_info.rs
@@ -2,10 +2,10 @@
 pub const PROGRAM_NAME: &str = "weather-cli";
 /// Description of the program.
 pub const PROGRAM_DESCRIPTION: &str =
-    "Minimalistic command-line weather program working with OpenWeather API.";
+    "Minimalistic command-line weather program. It works with OpenWeather API.";
 /// Top message in the help section.
 pub const ABOUT: &str =
-    "* weather-cli - Minimalistic command-line weather program working with OpenWeather API.";
+    "* weather-cli - Minimalistic command-line weather program. It works with OpenWeather API.";
 /// Author name of the program.
 pub const PROGRAM_AUTHORS: &str = "Stellar";
 /// URL of the program on crates.io.
